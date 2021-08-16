0bbaf5d6846b2308607fa5a6cce4342223f029e6
Peter Eliah Kodl <peter.e.kodl@gmail.com>
2021-08-16 19:41:00 +0200

    Added damping and restitution constants.

--- src/simulation.rs
+++ src/simulation.rs
@@ -11,6 +11,12 @@ pub type Vector2D = nalgebra::Vector2<f32>;
 
 pub mod constants
 {
+    use lazy_static::lazy_static;
     use std::time::Duration;
     pub const DT: Duration = Duration::from_millis(50);
+    const RESTITUTION: f32 = 0.1;
+    lazy_static! {
+        pub static ref DAMPING: f32 =
+            -RESTITUTION.ln() * (std::f32::consts::PI.powi(2) + RESTITUTION.ln().powi(2)).sqrt();
+    }
 }
